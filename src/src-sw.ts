7f56d3d8d4224cf5828dcf18fff021efc07fad7e
Eduardo Varela <eduardovarela139@gmail.com>
2022-12-16 17:14:53 -0600

    Development: Base configuration for PWA done

---
+++ src/src-sw.ts
@@ -0,0 +1,9 @@
+import { clientsClaim } from 'workbox-core';
+import { precacheAndRoute } from 'workbox-precaching';
+
+declare let self: ServiceWorkerGlobalScope;
+
+self.skipWaiting();
+clientsClaim();
+
+precacheAndRoute(self.__WB_MANIFEST);
